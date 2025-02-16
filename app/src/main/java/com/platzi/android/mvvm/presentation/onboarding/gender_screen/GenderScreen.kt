f2839fe6beeadb68930852f6c540f635de4fab1d
christian <cristhho@gmail.com>
2025-02-16 14:17:08 -0500

    gender screen

--- app/src/main/java/com/platzi/android/mvvm/presentation/onboarding/gender_screen/GenderScreen.kt
+++ app/src/main/java/com/platzi/android/mvvm/presentation/onboarding/gender_screen/GenderScreen.kt
@@ -9,14 +9,19 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import com.platzi.android.mvvm.app.R
 import com.platzi.android.mvvm.app.ui.theme.LocalSpacing
+import com.platzi.android.mvvm.presentation.onboarding.components.ActionButton
+import com.platzi.android.mvvm.presentation.onboarding.components.SelectableButton
 
 @Composable
 fun GenderScreen(
@@ -42,8 +47,44 @@ fun GenderScreen(
             )
             Spacer(modifier = Modifier.height(spacing.spaceMedium))
             Row {
-
+                SelectableButton(
+                    text = stringResource(id = R.string.male),
+                    isSelected = true,
+                    color = MaterialTheme.colorScheme.primary,
+                    selectedTextColor = Color.White,
+                    onClick = {},
+                    textStyle = MaterialTheme.typography.titleSmall.copy(
+                        fontWeight = FontWeight.Normal
+                    )
+                )
+                Spacer(modifier = Modifier.width(spacing.spaceMedium))
+                SelectableButton(
+                    text = stringResource(id = R.string.female),
+                    isSelected = false,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    selectedTextColor = Color.White,
+                    onClick = {},
+                    textStyle = MaterialTheme.typography.titleSmall.copy(
+                        fontWeight = FontWeight.Normal
+                    )
+                )
+                Spacer(modifier = Modifier.width(spacing.spaceMedium))
+                SelectableButton(
+                    text = stringResource(id = R.string.other),
+                    isSelected = false,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    selectedTextColor = Color.White,
+                    onClick = {},
+                    textStyle = MaterialTheme.typography.titleSmall.copy(
+                        fontWeight = FontWeight.Normal
+                    )
+                )
             }
         }
+        ActionButton(
+            text = stringResource(id = R.string.next),
+            onClick = { onNextClick() },
+            modifier = Modifier.align(Alignment.BottomEnd)
+        )
     }
 }
