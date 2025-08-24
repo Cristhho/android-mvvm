c4f7e11a5ba63e9cc426c735136794dd7e5f43f8
christian <cristhho@gmail.com>
2025-08-23 20:22:06 -0500

    overview meals items

--- app/src/main/java/com/platzi/android/mvvm/presentation/tracker_overview/TrackerOverviewScreen.kt
+++ app/src/main/java/com/platzi/android/mvvm/presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -1,17 +1,24 @@
 package com.platzi.android.mvvm.presentation.tracker_overview
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.platzi.android.mvvm.app.ui.theme.LocalSpacing
 import com.platzi.android.mvvm.app.ui.theme.PlatziCaloriesTheme
+import com.platzi.android.mvvm.presentation.tracker_overview.components.DaySelector
+import com.platzi.android.mvvm.presentation.tracker_overview.components.ExpandableMeal
 import com.platzi.android.mvvm.presentation.tracker_overview.components.NutrientHeader
+import com.platzi.android.mvvm.presentation.tracker_overview.model.defaultMeals
+import java.time.LocalDate
 
 @Composable
 fun TrackerOverviewScreen(
@@ -23,6 +30,31 @@ fun TrackerOverviewScreen(
         item {
             NutrientHeader()
             Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            DaySelector(
+                date = LocalDate.now(),
+                onPreviousDayClick = {
+
+                },
+                onNextDayClick = {
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = spacing.spaceMedium)
+            )
+        }
+        items(defaultMeals) { meal ->
+            ExpandableMeal(
+                meal = meal,
+                onToggleClick = {},
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = spacing.spaceSmall)
+                ) {  }
+            }
         }
     }
 }
@@ -34,6 +66,31 @@ fun TrackerOverviewScreenTest() {
         item {
             NutrientHeader()
             Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            DaySelector(
+                date = LocalDate.now(),
+                onPreviousDayClick = {
+
+                },
+                onNextDayClick = {
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = spacing.spaceMedium)
+            )
+        }
+        items(defaultMeals) { meal ->
+            ExpandableMeal(
+                meal = meal,
+                onToggleClick = {},
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = spacing.spaceSmall)
+                ) {  }
+            }
         }
     }
 }
